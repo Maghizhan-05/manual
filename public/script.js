700eadb5dec2344dd28463d0b830bd1f610c6c4d
maghizhan-05 <maghizhankamaraj@gmail.com>
2025-08-12 13:05:53 +0530

    Test: unprotected - protected link check

--- public/script.js
+++ public/script.js
@@ -3,7 +3,8 @@ const docFiles = [
   '/docs/operations/income_distribution.docx',
   '/docs/reports/admin.docx',
   '/docs/settlement/eod.docx',
-  '/docs/settelement/UTT- MFundPlus Implementation- FSD02- User Admin Masters (1) (1).docx'
+  '/docs/settelement/UTT- MFundPlus Implementation- FSD02- User Admin Masters (1) (1).docx',
+  '/docs/operations/excel_word.docx'
 ];
 
 /**
